592337dd5cc3e9a468b9e36c1591e73ab3eac19d
Regimantas Merkys <regimantas.merkys@gmail.com>
2017-10-20 13:53:20 +0300

    CartPage, CheckoutPage and ProductPage added
    getAvailability() and getCartCount() methods are available under
    product page

---
+++ test/pages/cart.page.js
@@ -0,0 +1,19 @@
+import Page from './page';
+import CheckoutPage from './checkout.page';
+
+class CartPage extends Page {
+  get checkoutButton() { return browser.element('[name="proceedToCheckout"]'); }
+
+  proceedToCheckout() {
+    this.open();
+    // this.checkoutButton.waitForVisible(3000);
+    this.checkoutButton.click();
+    return new CheckoutPage();
+  }
+
+  open() {
+    super.open('gp/cart/view.html/ref=nav_cart');
+  }
+}
+
+export default CartPage;
